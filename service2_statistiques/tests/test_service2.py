9dc585ddbc04eba3131e6fde29687abc2ccbecbb
DjibrilBerriche <djibril.berriche@etu.u-pec.fr>
2026-06-19 11:02:21 +0200

    Service2: mise a jour des tests Python

--- service2_statistiques/tests/test_service2.py
+++ service2_statistiques/tests/test_service2.py
@@ -2,20 +2,20 @@ import requests
 
 adresse = "http://127.0.0.1:5002"
 
-print("Test describe")
+print("Test /stats/describe")
 reponse = requests.post(adresse + "/stats/describe", json={
     "data": [12, 15, 8, 21, 13, 10]
 })
 print(reponse.json())
 
-print("Test correlation")
+print("Test /stats/correlation")
 reponse = requests.post(adresse + "/stats/correlation", json={
     "x": [1, 2, 3, 4],
     "y": [2, 4, 6, 8]
 })
 print(reponse.json())
 
-print("Test normalite")
+print("Test /stats/test_normalite")
 reponse = requests.post(adresse + "/stats/test_normalite", json={
     "data": [12, 13, 14, 15, 16, 14, 13, 15]
 })
